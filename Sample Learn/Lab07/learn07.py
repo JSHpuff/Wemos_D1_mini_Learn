2b5540d0bfa8d49745cb96dac7e810374634d482
jhenshou <82929215+JSHpuff@users.noreply.github.com>
2025-09-18 01:23:49 +0800

    update

--- Sample Learn/Lab07/learn07.py
+++ Sample Learn/Lab07/learn07.py
@@ -4,10 +4,10 @@ import time
 LED = PWM(Pin(15))
 
 while True:
-    for i in range(0, 1024, 10):
+    for i in range(200, 1024, 10):
         LED.duty(i)
         time.sleep(0.01)
     
-    for i in range(1023, -1, -10):
+    for i in range(1023, 199, -10):
         LED.duty(i)
         time.sleep(0.01)
