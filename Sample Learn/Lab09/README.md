47efc3c85728272b2781f81978f7c2ba5474d673
jhenshou <82929215+JSHpuff@users.noreply.github.com>
2025-09-25 00:38:48 +0800

    update lab 9

---
+++ Sample Learn/Lab09/README.md
@@ -0,0 +1,6 @@
+# Prepare
+1. LED * 7
+2. 220 ohms resistor * 7
+
+# Circuit
+1. Using D0, D1, D2, D5, D6, D7, D8 as ouput to LED.
---
+++ Sample Learn/Lab09/learn09.py
@@ -0,0 +1,18 @@
+from machine import Pin
+import time
+
+# 建立串列, 依序儲存 D0、D5、D6、D7、D8、D1、D2 腳位編號
+leds = [16, 14, 12, 13, 15, 5, 4]
+
+while True:
+    for i in leds:
+        led = Pin(i, Pin.OUT)
+        led.value(1)
+        time.sleep(0.1)
+        led.value(0)
+
+    for i in reversed(leds):
+        led = Pin(i, Pin.OUT)
+        led.value(1)
+        time.sleep(0.1)
+        led.value(0)
